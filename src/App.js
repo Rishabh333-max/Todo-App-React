93b15172b8fe421c48314ca5421dfc832372b5be
Rishabh333-max <81370987+Rishabh333-max@users.noreply.github.com>
2021-08-18 18:28:42 +0530

    Todo-App created by using React-->Props & Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+  const[name,setName]=useState(null);
+  const[fullName,setFullName]=useState([]);
+  const createTodo=(e)=>{
+ setName(e.target.value)
+  }
+  const setTodo=()=>{
+     setFullName((oldItems)=>{
+       return [...oldItems,name]
+     })
+     setName("");
+  }
+  const deleteItems=(id)=>{
+  setFullName((oldItems)=>{
+    return oldItems.filter((arrItems,index)=>{
+   return index!==id;
+    })
+  })
+  }
+  return(
+    <>
+    <div className="main_div">
+      <div className="center_div">
+        <br/>
+        <h1>Todo List</h1>
+        <br/>
+        <input type="text" placeholder="add a item" onChange={createTodo} value={name} />
+        <button onClick={setTodo}>+</button>
+
+        <ol>
+          {/* <li>{name}</li> */}
+        {  fullName.map((item,index)=>{
+    return  <TodoList key={index} 
+    id={index} 
+    text={item}
+    onSelect={deleteItems}  
+    />
+          })}
+        </ol>
+      </div>
     </div>
-  );
+    </>
+  )
 }
-
 export default App;
